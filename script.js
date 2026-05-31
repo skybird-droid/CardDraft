ebd59913ff63adf8109e51bfee09b4b6232c998f
skybird-droid <lubeskybird@gmail.com>
2026-05-31 17:50:42 -0400

    added everything so far

---
+++ script.js
@@ -0,0 +1,215 @@
+const lightSideCards = [
+    {image: "lightside/ls (1).jpg", name: "Atris", rarity: "rare" },
+  { image: "lightside/ls (2).jpg", name: "Bastila Shan", rarity: "epic" },
+  { image: "lightside/ls (3).jpg", name: "Dorak", rarity: "rare" },
+  { image: "lightside/ls (4).jpg", name: "Jolee Bindo", rarity: "rare" },
+  { image: "lightside/ls (5).jpg", name: "Juhani", rarity: "rare" },
+  { image: "lightside/ls (6).jpg", name: "Kao Cen Darach", rarity: "common" },
+  { image: "lightside/ls (7).jpg", name: "Kreia", rarity: "legendary" },
+  { image: "lightside/ls (8).jpg", name: "Meetra Surik", rarity: "epic" },
+  { image: "lightside/ls (9).jpg", name: "Revan", rarity: "legendary" },
+  { image: "lightside/ls (10).jpg", name: "Vandar Tokare", rarity: "rare" },
+  { image: "lightside/ls (11).jpg", name: "Ven Zallow", rarity: "common" },
+  { image: "lightside/ls (12).jpg", name: "Vrook Lamar", rarity: "rare" },
+  { image: "lightside/ls (13).jpg", name: "Zhar Lestin", rarity: "rare" },
+  { image: "lightside/ls (14).jpg", name: "Aayla Secura", rarity: "rare" },
+  { image: "lightside/ls (15).jpg", name: "Adi Gallia", rarity: "rare" },
+  { image: "lightside/ls (16).jpg", name: "Agen Kolar", rarity: "common" },
+  { image: "lightside/ls (17).jpg", name: "Ahsoka Tano", rarity: "legendary" },
+  { image: "lightside/ls (18).jpg", name: "Anakin Skywalker", rarity: "legendary" },
+  { image: "lightside/ls (19).jpg", name: "Barris Offee", rarity: "rare" },
+  { image: "lightside/ls (20).jpg", name: "Cin Drallig", rarity: "common" },
+  { image: "lightside/ls (21).jpg", name: "Coleman Trebor", rarity: "common" },
+  { image: "lightside/ls (22).jpg", name: "Depa Billaba", rarity: "rare" },
+  { image: "lightside/ls (23).jpg", name: "Eeth Koth", rarity: "common" },
+  { image: "lightside/ls (24).jpg", name: "Even Piell", rarity: "rare" },
+  { image: "lightside/ls (25).jpg", name: "Ima-Gun Di", rarity: "common" },
+  { image: "lightside/ls (26).jpg", name: "Jocasta Nu", rarity: "rare" },
+  { image: "lightside/ls (27).jpg", name: "Kelleran Beq", rarity: "common" },
+  { image: "lightside/ls (28).jpg", name: "Ki-Adi-Mundi", rarity: "rare" },
+  { image: "lightside/ls (29).jpg", name: "Kit Fisto", rarity: "epic" },
+  { image: "lightside/ls (30).jpg", name: "Luminara Unduli", rarity: "rare" },
+  { image: "lightside/ls (31).jpg", name: "Mace Windu", rarity: "epic" },
+  { image: "lightside/ls (32).jpg", name: "Nahdar Webb", rarity: "common" },
+  { image: "lightside/ls (33).jpg", name: "Obi-Wan Kenobi", rarity: "legendary" },
+  { image: "lightside/ls (34).jpg", name: "Oppo Rancisis", rarity: "common" },
+  { image: "lightside/ls (35).jpg", name: "Plo Koon", rarity: "epic" },
+  { image: "lightside/ls (36).jpg", name: "Qui-Gon Jinn", rarity: "epic" },
+  { image: "lightside/ls (37).jpg", name: "Quinlan Vos", rarity: "rare" },
+  { image: "lightside/ls (38).jpg", name: "Saesee Tiin", rarity: "common" },
+  { image: "lightside/ls (39).jpg", name: "Shaak Ti", rarity: "rare" },
+  { image: "lightside/ls (40).jpg", name: "Stass Allie", rarity: "common" },
+  { image: "lightside/ls (41).jpg", name: "Tera Sinube", rarity: "common" },
+  { image: "lightside/ls (42).jpg", name: "Yaddle", rarity: "rare" },
+  { image: "lightside/ls (43).jpg", name: "Yarael Poof", rarity: "common" },
+  { image: "lightside/ls (44).jpg", name: "Yoda", rarity: "legendary" },
+  { image: "lightside/ls (45).jpg", name: "Dooku", rarity: "rare" },
+  { image: "lightside/ls (46).jpg", name: "A'Sharad Hett", rarity: "rare" },
+  { image: "lightside/ls (47).jpg", name: "Ben Kenobi", rarity: "epic" },
+  { image: "lightside/ls (48).jpg", name: "Cade Skywalker", rarity: "rare" },
+  { image: "lightside/ls (49).jpg", name: "Cal Kestis", rarity: "epic" },
+  { image: "lightside/ls (50).jpg", name: "Caleb Dume", rarity: "common" },
+  { image: "lightside/ls (51).jpg", name: "Corran Horn", rarity: "common" },
+  { image: "lightside/ls (52).jpg", name: "Ezra Bridger", rarity: "rare" },
+  { image: "lightside/ls (53).jpg", name: "Galen Marek", rarity: "rare" },
+  { image: "lightside/ls (54).jpg", name: "Jaro Tapal", rarity: "common" },
+  { image: "lightside/ls (55).jpg", name: "Kanan Jarrus", rarity: "rare" },
+  { image: "lightside/ls (56).jpg", name: "Kento Marek", rarity: "common" },
+  { image: "lightside/ls (57).jpg", name: "Kyle Katarn", rarity: "rare" },
+  { image: "lightside/ls (58).jpg", name: "Luke Skywalker", rarity: "legendary" },
+  { image: "lightside/ls (59).jpg", name: "Mara Jade", rarity: "epic" },
+  { image: "lightside/ls (60).jpg", name: "Qu Rahn", rarity: "common" },
+  { image: "lightside/ls (61).jpg", name: "Rahm Kota", rarity: "rare" },
+  { image: "lightside/ls (62).jpg", name: "Rosh Penin", rarity: "common" },
+  { image: "lightside/ls (63).jpg", name: "Grogu", rarity: "epic" },
+  { image: "lightside/ls (64).jpg", name: "Luke Skywalker (Hermit)", rarity: "epic" },
+  { image: "lightside/ls (65).jpg", name: "Rey", rarity: "legendary" }
+];
+
+const darkSideCards = [
+  { image: "darkside/ds- (1).jpg", name: "Bastila Shan (Dark Side)", rarity: "epic" },
+{ image: "darkside/ds- (2).jpg", name: "Arcann", rarity: "rare" },
+{ image: "darkside/ds- (3).jpg", name: "Darth Bandon", rarity: "common" },
+{ image: "darkside/ds- (4).jpg", name: "Darth Bane", rarity: "legendary" },
+{ image: "darkside/ds- (5).jpg", name: "Darth Malgus", rarity: "legendary" },
+{ image: "darkside/ds- (6).jpg", name: "Darth Malak", rarity: "epic" },
+{ image: "darkside/ds- (7).jpg", name: "Darth Nihilus", rarity: "epic" },
+{ image: "darkside/ds- (8).jpg", name: "Darth Revan", rarity: "legendary" },
+{ image: "darkside/ds- (9).jpg", name: "Revan Reborn", rarity: "legendary" },
+{ image: "darkside/ds- (10).jpg", name: "Darth Sion", rarity: "rare" },
+{ image: "darkside/ds- (11).jpg", name: "Darth Traya", rarity: "legendary" },
+{ image: "darkside/ds- (12).jpg", name: "Darth Vindican", rarity: "common" },
+{ image: "darkside/ds- (13).jpg", name: "Emperor Valkorion", rarity: "legendary" },
+{ image: "darkside/ds- (14).jpg", name: "Jorak Uln", rarity: "common" },
+{ image: "darkside/ds- (15).jpg", name: "Lord Scourge", rarity: "rare" },
+{ image: "darkside/ds- (16).jpg", name: "Thexan", rarity: "rare" },
+{ image: "darkside/ds- (17).jpg", name: "Uthar Wynn", rarity: "common" },
+{ image: "darkside/ds- (18).jpg", name: "Visas Marr", rarity: "rare" },
+{ image: "darkside/ds- (19).jpg", name: "Yuthura Ban", rarity: "rare" },
+{ image: "darkside/ds- (20).jpg", name: "Asajj Ventress", rarity: "epic" },
+{ image: "darkside/ds- (21).jpg", name: "Count Dooku", rarity: "epic" },
+{ image: "darkside/ds- (22).jpg", name: "Darth Maul", rarity: "epic" },
+{ image: "darkside/ds- (23).jpg", name: "Darth Plaguesis", rarity: "rare" },
+{ image: "darkside/ds- (24).jpg", name: "Darth Sidious", rarity: "legendary" },
+{ image: "darkside/ds- (25).jpg", name: "General Grievous", rarity: "rare" },
+{ image: "darkside/ds- (26).jpg", name: "Lord Vader", rarity: "legendary" },
+{ image: "darkside/ds- (27).jpg", name: "Mother Talzin", rarity: "rare" },
+{ image: "darkside/ds- (28).jpg", name: "Savage Opress", rarity: "rare" },
+{ image: "darkside/ds- (29).jpg", name: "Alora", rarity: "common" },
+{ image: "darkside/ds- (30).jpg", name: "Baylan Skoll", rarity: "rare" },
+{ image: "darkside/ds- (31).jpg", name: "Boc", rarity: "common" },
+{ image: "darkside/ds- (32).jpg", name: "Dagan Gera", rarity: "rare" },
+{ image: "darkside/ds- (33).jpg", name: "Darth Desolous", rarity: "common" },
+{ image: "darkside/ds- (34).jpg", name: "Darth Krayt", rarity: "rare" },
+{ image: "darkside/ds- (35).jpg", name: "Darth Phobos", rarity: "common" },
+{ image: "darkside/ds- (36).jpg", name: "Darth Talon", rarity: "rare" },
+{ image: "darkside/ds- (37).jpg", name: "Darth Vader", rarity: "legendary" },
+{ image: "darkside/ds- (38).jpg", name: "Desann", rarity: "rare" },
+{ image: "darkside/ds- (39).jpg", name: "Emperor Palpatine", rarity: "legendary" },
+{ image: "darkside/ds- (40).jpg", name: "Gorc", rarity: "common" },
+{ image: "darkside/ds- (41).jpg", name: "Jerec", rarity: "rare" },
+{ image: "darkside/ds- (42).jpg", name: "Maw", rarity: "common" },
+{ image: "darkside/ds- (43).jpg", name: "Pic", rarity: "common" },
+{ image: "darkside/ds- (44).jpg", name: "Sariss", rarity: "common" },
+{ image: "darkside/ds- (45).jpg", name: "Shin Hati", rarity: "rare" },
+{ image: "darkside/ds- (46).jpg", name: "Starkiller", rarity: "epic" },
+{ image: "darkside/ds- (47).jpg", name: "Taron Malicos", rarity: "rare" },
+{ image: "darkside/ds- (48).jpg", name: "Tavion Axmis", rarity: "rare" },
+{ image: "darkside/ds- (49).jpg", name: "The Grand Inquisitor", rarity: "rare" },
+{ image: "darkside/ds- (50).jpg", name: "The Eighth Brother", rarity: "common" },
+{ image: "darkside/ds- (51).jpg", name: "The Fifth Brother", rarity: "common" },
+{ image: "darkside/ds- (52).jpg", name: "The Ninth Sister", rarity: "common" },
+{ image: "darkside/ds- (53).jpg", name: "The Second Sister", rarity: "rare" },
+{ image: "darkside/ds- (54).jpg", name: "The Seventh Sister", rarity: "common" },
+{ image: "darkside/ds- (55).jpg", name: "The Third Sister", rarity: "common" },
+{ image: "darkside/ds- (56).jpg", name: "Yun", rarity: "common" },
+{ image: "darkside/ds- (57).jpg", name: "Emperor Palpatine (Reborn)", rarity: "legendary" },
+{ image: "darkside/ds- (58).jpg", name: "Kylo Ren", rarity: "legendary" },
+{ image: "darkside/ds- (59).jpg", name: "Ren", rarity: "common" },
+{ image: "darkside/ds- (60).jpg", name: "Supreme Leader Snoke", rarity: "rare" }
+];
+
+let gameStarted = false;
+
+const rarityChances = {
+    common: 60,
+    rare: 25,
+    epic: 10,
+    legendary: 5
+};
+
+function getRandomCards(array, count) {
+    const pool = [...array];
+    const selected = [];
+
+    while (selected.length < count && pool.length > 0) {
+        const totalWeight = pool.reduce((sum, card) => {
+            return sum + (rarityChances[card.rarity] || 1);
+        }, 0);
+
+        let random = Math.random() * totalWeight;
+        let index = 0;
+
+        for (; index < pool.length; index++) {
+            const weight = rarityChances[pool[index].rarity] || 1;
+            random -= weight;
+            if (random < 0) break;
+        }
+
+        if (index >= pool.length) {
+            index = pool.length - 1;
+        }
+
+        selected.push(pool.splice(index, 1)[0]);
+    }
+
+    return selected;
+}
+
+function showCards(side) {
+
+    if (gameStarted) return;
+
+    gameStarted = true;
+
+    const container = document.getElementById("cardContainer");
+    container.innerHTML = "";
+
+    const selectedCards =
+        side === "light"
+            ? getRandomCards(lightSideCards, 4)
+            : getRandomCards(darkSideCards, 4);
+
+    selectedCards.forEach(cardData => {
+
+        const card = document.createElement("div");
+        card.classList.add("card", "face-down");
+
+        const img = document.createElement("img");
+        img.src = cardData.image;
+        img.style.display = "none";
+
+        card.appendChild(img);
+
+        card.addEventListener("click", () => {
+
+            if (card.classList.contains("revealed")) return;
+
+            card.classList.add("revealed");
+            card.classList.add(cardData.rarity);
+            card.classList.remove("face-down");
+
+            img.style.display = "block";
+
+            const name = document.createElement("div");
+            name.classList.add("character-name");
+            name.textContent = cardData.name;
+
+            card.appendChild(name);
+        });
+
+        container.appendChild(card);
+    });
+
+    document.querySelector(".button-container").style.display = "none";
+}
